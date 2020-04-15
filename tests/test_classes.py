33a66aba3eba7373be9d1e6099cc48d0a63759b7
Adam Morrissett <me@adamlm.com>
2020-04-14 21:28:23 -0400

    Add test case for RansacParams data class
    - Add test for class initialization

---
+++ tests/test_classes.py
@@ -0,0 +1,34 @@
+"""class module tests.
+
+This module contains tests for classes in the classes module.
+"""
+
+# Standard library imports
+import unittest
+
+# Local application imports
+import ransac
+
+
+class TestRansacParams(unittest.TestCase):
+    """Test RansacParams data class.
+
+    """
+    def test_init(self) -> None:
+        """Test RansacParams initialization.
+
+        :return: None
+        """
+        params = ransac.RansacParams(samples=1,
+                                     iterations=2,
+                                     confidence=3,
+                                     threshold=4)
+
+        self.assertEqual(params.samples, 1)
+        self.assertEqual(params.iterations, 2)
+        self.assertEqual(params.confidence, 3)
+        self.assertEqual(params.threshold, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
