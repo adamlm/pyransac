4ec11993461b56368f7c75e8672fabdef1af71e4
Adam Morrissett <me@adamlm.com>
2020-04-15 15:11:55 -0400

    Move RANSAC core code into a single module
    - Move RansacParams into ransac module
    - Move find_inliers function into ransac module
    - Move RANSAC tests into test_ransac module

--- tests/test_ransac.py
+++ tests/test_ransac.py
@@ -1,23 +1,40 @@
+"""Test case for the ransac module.
+
+This module contains tests for the ransac module.
+"""
+
+# Standard library imports
 import math
 import unittest
 
-import pyransac
+# Local application imports
+from pyransac import ransac
 
 
 class TestRansac(unittest.TestCase):
-    def setUp(self) -> None:
-        self.params = pyransac.RansacParams(samples=1,
-                                            iterations=2,
-                                            confidence=3,
-                                            threshold=4)
+    """Test the ransac module.
 
+    """
     def test_ransac_params(self) -> None:
-        self.assertEqual(self.params.samples, 1)
-        self.assertEqual(self.params.iterations, 2)
-        self.assertEqual(self.params.confidence, 3)
-        self.assertEqual(self.params.threshold, 4)
+        """Test RansacParams initialization.
+
+        :return: None
+        """
+        params = ransac.RansacParams(samples=1,
+                                     iterations=2,
+                                     confidence=3,
+                                     threshold=4)
+
+        self.assertEqual(params.samples, 1)
+        self.assertEqual(params.iterations, 2)
+        self.assertEqual(params.confidence, 3)
+        self.assertEqual(params.threshold, 4)
 
     def test_find_inliers(self) -> None:
+        """Test the ransac find_inliers function.
+
+        :return: None
+        """
         def param_func(points):
             point1 = points[0]
             point2 = points[1]
@@ -53,20 +70,21 @@ class TestRansac(unittest.TestCase):
             if params['slope'] is math.nan:
                 return abs(a * point[0]) / abs(a)
 
-            return abs(a * point[0] + b * point[1] + c) / math.sqrt(a**2 + b**2)
+            return abs(a * point[0] + b * point[1] + c) / math.sqrt(
+                a ** 2 + b ** 2)
 
         test_inliers = [(x, x) for x in range(0, 10)]
         test_outliers = [(5, 1), (5, 2), (6, 1), (5, 2)]
         test_data = test_inliers + test_outliers
-        ransac_params = pyransac.RansacParams(samples=2,
-                                              iterations=10,
-                                              confidence=0.5,
-                                              threshold=1)
+        ransac_params = ransac.RansacParams(samples=2,
+                                            iterations=10,
+                                            confidence=0.5,
+                                            threshold=1)
 
-        inliers = pyransac.find_inliers(points=test_data,
-                                        param_func=param_func,
-                                        error_func=error_func,
-                                        params=ransac_params)
+        inliers = ransac.find_inliers(points=test_data,
+                                      param_func=param_func,
+                                      error_func=error_func,
+                                      params=ransac_params)
 
         self.assertEqual(sorted(test_inliers), sorted(inliers))
 
